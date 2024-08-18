a74cf556a6f35442f6ce0a9b0c1c7d1262016072
Arjun <122396184+arjunrawal@users.noreply.github.com>
2024-08-18 16:06:38 +0545

    Update student.py
    right frame and necessary things

--- student.py
+++ student.py
@@ -57,14 +57,14 @@ class Student:
         Left_frame=LabelFrame(main_frame,bd=2,bg="white",relief=RIDGE,text="Student Details",font=("Roboto",12,"bold"))
         Left_frame.place(x=10, y=10, width=760,height=580)
 
-     #loading and resizing the image
+     #loading and resizing the image in left label
         img_left = Image.open(r"C:\Users\aaaar\Desktop\FaceRecognitionSystem\college_images\Student4.jpg")
         img_left = img_left.resize((750, 130), Image.LANCZOS)
         self.photoimg_left = ImageTk.PhotoImage(img_left)
 
-    #to display the image in a label
+    #to display the image in a left label
         f_lbl = Label(Left_frame, image=self.photoimg_left)
-        f_lbl.place(x=5, y=0, width=750, height=130)
+        f_lbl.place(x=1, y=0, width=750, height=130)
 
 
     #Current course
@@ -91,110 +91,179 @@ class Student:
 
     #Class Student information
         Class_Student_frame=LabelFrame(Left_frame,bd=2,bg="white",relief=RIDGE,text="Class Student Information ",font=("Roboto",13,"bold"),)
-        Class_Student_frame.place(x=5, y=220, width=750,height=300)
+        Class_Student_frame.place(x=5, y=220, width=750,height=320)
 
     #Student id
-        studentId_label=Label(Class_Student_frame,text="StudentID:",font=("roboto",13,"bold"),bg="white")
+        studentId_label=Label(Class_Student_frame,text="Student_ID :",font=("roboto",13,"bold"),bg="white")
         studentId_label.grid(row=0,column=0,padx=10,pady=5,sticky=W)
 
         studentId_entry=ttk.Entry(Class_Student_frame,width=20,font=("roboto",13,"bold"))
         studentId_entry.grid(row=0,column=1,padx=10,pady=5,sticky=W)
     
     #Student name
-        studentName_label=Label(Class_Student_frame,text="Student Name:",font=("roboto",13,"bold"),bg="white")
+        studentName_label=Label(Class_Student_frame,text="Student Name :",font=("roboto",13,"bold"),bg="white")
         studentName_label.grid(row=0,column=2,padx=10,pady=5,sticky=W)
 
         studentName_entry=ttk.Entry(Class_Student_frame,width=20,font=("roboto",13,"bold"))
         studentName_entry.grid(row=0,column=3,padx=10,pady=5,sticky=W)
 
     #Student roll no
-        roll_no_label=Label(Class_Student_frame,text="Roll No:",font=("roboto",13,"bold"),bg="white")
+        roll_no_label=Label(Class_Student_frame,text="Roll No :",font=("roboto",13,"bold"),bg="white")
         roll_no_label.grid(row=1,column=0,padx=10,pady=5,sticky=W)
 
         roll_no_entry=ttk.Entry(Class_Student_frame,width=20,font=("roboto",13,"bold"))
         roll_no_entry.grid(row=1,column=1,padx=10,pady=5,sticky=W)
 
     #Student Date of Birth
-        dob_label=Label(Class_Student_frame,text="Date Of Birth:",font=("roboto",13,"bold"),bg="white")
+        dob_label=Label(Class_Student_frame,text="Date Of Birth :",font=("roboto",13,"bold"),bg="white")
         dob_label.grid(row=1,column=2,padx=10,pady=5,sticky=W)
 
         dob_entry=ttk.Entry(Class_Student_frame,width=20,font=("roboto",13,"bold"))
         dob_entry.grid(row=1,column=3,padx=10,pady=5,sticky=W)
 
     #Gender Of student
-        gender_label=Label(Class_Student_frame,text="Gender:",font=("roboto",13,"bold"),bg="white")
+        gender_label=Label(Class_Student_frame,text="Gender :",font=("roboto",13,"bold"),bg="white")
         gender_label.grid(row=2,column=0,padx=10,pady=5,sticky=W)
 
-        gender_entry=ttk.Entry(Class_Student_frame,width=20,font=("roboto",13,"bold"))
-        gender_entry.grid(row=2,column=1,padx=10,pady=5,sticky=W)
+    #making choosing like options
+        gender_combo=ttk.Combobox(Class_Student_frame,font=("times new roman",12,"bold"),state="readonly")
+        gender_combo["values"]=("Select Here:","Male","Female","Others")
+        gender_combo.current(0)
+        gender_combo.grid(row=2,column=1,padx=10,pady=5,sticky=W)
 
+        
     #Address Of student
-        address_label=Label(Class_Student_frame,text="Address:",font=("roboto",13,"bold"),bg="white")
-        address_label.grid(row=2,column=0,padx=10,pady=5,sticky=W)
+        address_label=Label(Class_Student_frame,text="Address :",font=("roboto",13,"bold"),bg="white")
+        address_label.grid(row=2,column=2,padx=10,pady=5,sticky=W)
 
         address_entry=ttk.Entry(Class_Student_frame,width=20,font=("roboto",13,"bold"))
-        address_entry.grid(row=2,column=1,padx=10,pady=5,sticky=W)
+        address_entry.grid(row=2,column=3,padx=10,pady=5,sticky=W)
 
     #Contact Number
-        contactNo_label=Label(Class_Student_frame,text="Contact:",font=("roboto",13,"bold"),bg="white")
-        contactNo_label.grid(row=2,column=2,padx=10,pady=5,sticky=W)
+        contactNo_label=Label(Class_Student_frame,text="Contact :",font=("roboto",13,"bold"),bg="white")
+        contactNo_label.grid(row=3,column=0,padx=10,pady=5,sticky=W)
 
         contacNo_entry=ttk.Entry(Class_Student_frame,width=20,font=("roboto",13,"bold"))
-        contacNo_entry.grid(row=2,column=3,padx=10,pady=5,sticky=W)
+        contacNo_entry.grid(row=3,column=1,padx=10,pady=5,sticky=W)
 
-    #radio Buttons
-        radiobutton1=ttk.Radiobutton(Class_Student_frame,text="Take photo sample",value="Yes")
-        radiobutton1.grid(row=6,column=0)
+    #student E-mail:
+        email_label=Label(Class_Student_frame,text="E-mail :",font=("roboto",13,"bold"),bg="white")
+        email_label.grid(row=3, column=2,padx=10, pady=5, sticky=W)
 
-        radiobutton1=ttk.Radiobutton(Class_Student_frame,text="No photo sample",value="Yes")
-        radiobutton1.grid(row=6,column=1)
+        email_entry=ttk.Entry(Class_Student_frame,width=20,font=("times new roman",13,"bold"))
+        email_entry.grid(row=3,column=3,padx=10,pady=5,sticky=W)
+ 
+    #Teacher name:
+        teacherName_label=Label(Class_Student_frame,text="Teacher Name :",font=("roboto",13,"bold"),bg="white")
+        teacherName_label.grid(row=4, column=0,padx=10, pady=5, sticky=W)
 
-    #Button frame
-        btn_frame=Frame(Class_Student_frame,bd=2,relief=RIDGE,bg="white")
-        btn_frame.place(x=0,y=130,width=750,height=80)
-
-        save_btn=Button(btn_frame,text="Save",width="18",font=("roboto",13,"bold"),bg="blue",fg="white")
-        save_btn.grid(row=0,column=0)
-
-        update_btn=Button(btn_frame,text="Update",width="18",font=("roboto",13,"bold"),bg="blue",fg="white")
-        update_btn.grid(row=0,column=1)
-
-        delete_btn=Button(btn_frame,text="Delete",width="18",font=("roboto",13,"bold"),bg="blue",fg="white")
-        delete_btn.grid(row=0,column=2)
-
-        reset_btn=Button(btn_frame,text="Reset",width="18",font=("roboto",13,"bold"),bg="blue",fg="white")
-        reset_btn.grid(row=0,column=3)
-
-
-    # #another frame
-    #     btn_frame1=Frame(Class_Student_frame,bd=2,relief=RIDGE,bg="white")
-    #     btn_frame1.place(x=0,y=235,width=715,height=35)
-
-        take_photo_btn=Button(btn_frame,text="Take Photo Sample",width="18",font=("roboto",13,"bold"),bg="blue",fg="white")
-        take_photo_btn.grid(row=1,column=0)
-
-        update_photo_btn=Button(btn_frame,text="Update Photo Sample",width="18",font=("roboto",13,"bold"),bg="blue",fg="white")
-        update_photo_btn.grid(row=1,column=1)
-
-
-
-
-
-
-
-
-
+        teacherName_entry=ttk.Entry(Class_Student_frame,width=20,font=("times new roman",13,"bold"))
+        teacherName_entry.grid(row=4,column=1,padx=10,pady=5,sticky=W)
     
+    #Teacher subject name:
+        teacher_subject_label=Label(Class_Student_frame,text="Teacher's subject Name :",font=("roboto",13,"bold"),bg="white")
+        teacher_subject_label.grid(row=4, column=2,padx=10, pady=5, sticky=W)
+
+        teacher_subject_entry=ttk.Entry(Class_Student_frame,width=20,font=("times new roman",13,"bold"))
+        teacher_subject_entry.grid(row=4,column=3,padx=10,pady=5,sticky=W)
 
 
+    #radio button
+        radiobtn1=ttk.Radiobutton(Class_Student_frame,text="Take Photo Sample",value="Yes")
+        radiobtn1.grid(row=6,padx=5,pady=0,column=0)
+
+        radiobtn2=ttk.Radiobutton(Class_Student_frame,text="No Photo Sample")
+        radiobtn2.grid(row=6,padx=5,pady=0,column=1)
+
+    # Buttons Frame
+        btn_frame = Frame(Class_Student_frame, bd=2, relief=RIDGE, bg="white")
+        btn_frame.place(x=0, y=200, width=760, height=130)
+
+    # Row 1Buttons
+        save_btn = Button(btn_frame, text="Save", width=15, font=("times new roman", 13, "bold"), bg="green", fg="white")
+        save_btn.grid(row=0, column=0, padx=5, pady=5)
+       
+        update_btn = Button(btn_frame, text="Update", width=15, font=("times new roman", 13, "bold"), bg="yellow", fg="black")
+        update_btn.grid(row=0, column=1, padx=5, pady=5)
+       
+        delete_btn = Button(btn_frame, text="Delete", width=15, font=("times new roman", 13, "bold"), bg="red", fg="white")
+        delete_btn.grid(row=0, column=2, padx=5, pady=5)
+       
+        reset_btn = Button(btn_frame, text="Reset", width=15, font=("times new roman", 13, "bold"), bg="blue", fg="white")
+        reset_btn.grid(row=0, column=3, padx=5, pady=5)
+    
+     # Row 2 Buttons
+        take_photo_btn = Button(btn_frame, text="Take Photo Sample", width=20, font=("times new roman", 13, "bold"), bg="blue", fg="white")
+        take_photo_btn.grid(row=1, column=0, columnspan=2, padx=5, pady=5)
+       
+        update_photo_btn = Button(btn_frame, text="Update Photo Sample", width=20, font=("times new roman", 13, "bold"), bg="blue", fg="white")
+        update_photo_btn.grid(row=1, column=2, columnspan=2, padx=5, pady=5)
+
+
+
+#==========End of left side================
 
 
 
      #Right label frame  
-        Right=LabelFrame(main_frame,bd=2,bg="white",relief=RIDGE,text="Student Details",font=("Roboto",12,"bold"))
-        Right.place(x=780, y=10, width=680,height=580)
+        Right_frame=LabelFrame(main_frame,bd=2,bg="white",relief=RIDGE,text="Student Details",font=("Roboto",12,"bold"))
+        Right_frame.place(x=780, y=10, width=680,height=580)
 
+        
+     #loading and resizing the image of right frame
+        img_Right = Image.open(r"C:\Users\aaaar\Desktop\FaceRecognitionSystem\college_images\Right1.jpg")
+        img_Right = img_Right.resize((750, 130), Image.LANCZOS)
+        self.photoimg_Right = ImageTk.PhotoImage(img_Right)
+
+    #to display the image in a  right label
+        Right_lbl = Label(Right_frame, image=self.photoimg_Right)
+        Right_lbl.place(x=1, y=0, width=680, height=130)
+
+
+    #=====Searching System=============
+
+
+        Search_frame=LabelFrame(Right_frame,bd=2,bg="white",relief=RIDGE,text="Search System ",font=("Roboto",13,"bold"),)
+        Search_frame.place(x=5, y=135, width=710,height=70)
+
+
+        search_label=Label(Search_frame,text="Search By :",font=("roboto",13,"bold"),bg="yellow",fg="blue")
+        search_label.grid(row=0,column=0,padx=10,pady=5,sticky=W)
+
+
+        search_combo=ttk.Combobox(Search_frame,font=("Roboto",12,"bold"),state="readonly",width=12)
+        search_combo["values"]=("Select ","Roll No ","Phone Number","StudentID")
+        search_combo.current(0)
+        search_combo.grid(row=0,column=1,padx=2,pady=10,sticky=W)
+        
+        search_entry=ttk.Entry(Search_frame,width=15,font=("times new roman",12,"bold"))
+        search_entry.grid(row=0,column=2,padx=10,pady=4,sticky=W)
     
+        search_btn = Button(Search_frame, text="Search", width=12, font=("times new roman", 13, "bold"), bg="blue", fg="white")
+        search_btn.grid(row=0, column=3, padx=4, pady=5)
+
+        showAll_btn = Button(Search_frame, text="Show All", width=12, font=("times new roman", 13, "bold"), bg="green", fg="white")
+        showAll_btn.grid(row=0, column=4, padx=4, pady=5)
+        #=========== table frame===========
+
+
+        table_frame=LabelFrame(Right_frame,bd=2,bg="white",relief=RIDGE)
+        table_frame.place(x=5, y=210, width=680,height=250)
+
+
+        #creating scroll bar
+
+        scroll_x=ttk.Scrollbar(table_frame,orient=HORIZONTAL)
+        scroll_y=ttk.Scrollbar(table_frame,orient=VERTICAL)
+
+        self.student_table=ttk.Treeview(table_frame,column=("dep","course","year","sem","id","name","rollno","gender","email","phone","photo"),xscrollcommand=scroll_x.set,yscrollcommand=scroll_y.set)
+
+        scroll_x.pack(side=BOTTOM,fill=X)
+        scroll_y.pack(side=RIGHT,fill=Y)
+
+        
+
+
 
 
 
