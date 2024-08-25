f952140a6f682cd64d7e44bdbf66ec9fdaccf185
Arjun <122396184+arjunrawal@users.noreply.github.com>
2024-08-25 21:56:36 +0545

    Update student.py
    facebutton with opeining camera

--- student.py
+++ student.py
@@ -248,7 +248,7 @@ class Student:
         reset_btn.grid(row=0, column=3, padx=5, pady=5)
     
      # Row 2 Buttons
-        take_photo_btn = Button(btn_frame, text="Take Photo Sample",cursor="hand2", width=20, font=("times new roman", 13, "bold"), bg="blue", fg="white")
+        take_photo_btn = Button(btn_frame,command==self.generate_dataset,text="Take Photo Sample",cursor="hand2", width=20, font=("times new roman", 13, "bold"), bg="blue", fg="white")
         take_photo_btn.grid(row=1, column=0, columnspan=2, padx=5, pady=5)
        
         update_photo_btn = Button(btn_frame, text="Update Photo Sample",cursor="hand2", width=20, font=("times new roman", 13, "bold"), bg="blue", fg="white")
@@ -556,6 +556,45 @@ class Student:
                 self.fetch_data()
                 self.reset_data()
                 conn.close()
+
+
+
+                #======load Predefined data on face frontals form opencv======
+                face_classifier=cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+
+                def face_cropped(img):
+                    gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+                    faces=face_classifier.detectMultiScale(gray,1.3,5)
+                    #scaling factor =1.3
+                    #minimum Neighbor=5
+
+                    for(x,y,w,y) in faces:
+                        face_cropped=img[y:y+h,x:x+w]
+                        return face_cropped
+                    
+                cap=cv2.VideoCapture(0)
+                img_id=0
+                while True:
+                    ret,my_frame=cap.read()
+                    if face_cropped(my_frame) is not None:
+                        img_id+=1
+                    face=cv2.resize(face_cropped(my_frame),(450,450))
+                    face=cv2.cvtColor(face,cv2.COLOR_BGR2GRAY)
+                    file_name_path="data/user."+str(id)+"."+str(img_id)+".jpg"
+                    cv2.imwrite(file_name_path)
+                    cv2.putText(face,str(img_id),cv2.FONT_HERSHEY_COMPLEX,2,(0,255,0),2)
+                    cv2.imshow("Crooped Face",face)
+
+                    if cv2.waitKey(1)==13 or int(img_id)==100:
+                        break
+                cap.release()
+                cv2.destroyAllWindows()
+                messagebox.showinfo("Result","Generating data sets completed !!!!!")
+
+            except EXCEPTION as es:
+                messagebox.showerror("Error",f"Due To:{str(es)}",parent=self.root)
+
+
 
 
 
