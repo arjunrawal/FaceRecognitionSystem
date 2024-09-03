d45c5bd52f886ca4d61ec3330e058909fcb36b6d
Arjun <122396184+arjunrawal@users.noreply.github.com>
2024-09-03 11:49:52 +0545

    Update face_recognition.py
    face recognized

--- face_recognition.py
+++ face_recognition.py
@@ -45,22 +45,69 @@ class Face_Recognition:
 
 
         #button
-        b1_1 = Button(f_lbl, text="Face Recognition", cursor="hand2", font=("Roboto", 18, "bold"), bg="blue", fg="white")
+        b1_1 = Button(f_lbl, text="Face Recognition", command=self.face_recog,cursor="hand2", font=("Roboto", 18, "bold"), bg="blue", fg="white")
         b1_1.place(x=350, y=600, width=200, height=40)
 
 
+    #=============face recognition========
+    def face_recog(self):
+        def draw_boundry(img,classifier,scaleFactor,minNeighbors,color,text,clf):
+            gray_image=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+            features=classifier.detectMultiScale(gray_image,scaleFactor,minNeighbors)
+
+            coord=[]
+
+            for (x,y,w,h) in features:
+                cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),3)
+                id,predict=clf.predict(gray_image[y:y+h,x:x+w])
+                confidence=int((100*(1-predict/300)))
+
+                conn = mysql.connector.connect(host="localhost", username="root", password="sql123", database="face_recognizer")
+                my_cursor = conn.cursor()
+
+                my_cursor.execute("select name from Student where id="+str(id))
+                n=my_cursor.fetchone()
+                n="+".join(n)
+
+                my_cursor.execute("select rollno from Student where id="+str(id))
+                r=my_cursor.fetchone()
+                r="+".join(r)
+
+                my_cursor.execute("select dep from Student where id="+str(id))
+                d=my_cursor.fetchone()
+                d="+".join(d)
 
 
+                if confidence > 77: 
+                    cv2.putText(img,f"Roll:{r}",(x,y-55),cv2.FONT_HERSHEY_COMPLEX,0.8,(255,255,255),3)
+                    cv2.putText(img,f"Name:{n}",(x,y-35),cv2.FONT_HERSHEY_COMPLEX,0.8,(255,255,255),3)
+                    cv2.putText(img,f"department:{d}",(x,y-5),cv2.FONT_HERSHEY_COMPLEX,0.8,(255,255,255),3)
 
+                else:
+                    cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),3)
+                    cv2.putText(img,"Unknown Face:",(x,y-35),cv2.FONT_HERSHEY_COMPLEX,0.8,(255,255,255),3)
 
+                coord=[x,y,w,h]
+            return coord
+        def recognize(img,clf,faceCascade):
+            coord=draw_boundry(img,faceCascade,1.1,10,(255,255,255),"Face",clf)
+            return img
+        
+        faceCascade=cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+        clf=cv2.face.LBPHFaceRecognizer_create()
+        clf.read("classifier.xml")
 
+        video_cap=cv2.VideoCapture(0)
 
+        while True:
+            ret,img=video_cap.read()
+            img=recognize(img,clf,faceCascade)
+            cv2.imshow("Welcome To Face Recognition",img)
 
-
-
-
-
-
+            if cv2.waitKey(1)==13:
+                break
+        video_cap.release()
+        cv2.destroyAllWindows()
 
 
 
